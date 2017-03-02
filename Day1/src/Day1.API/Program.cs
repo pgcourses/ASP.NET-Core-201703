d369a6715f0053f28a53812159f15a94d8ab650b
gplesz <gabor.plesz@gmail.com>
2017-03-02 13:31:44 +0100

    #1: hello world alkalmazás

---
+++ Day1/src/Day1.API/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+/// <summary>
+/// .NET Core
+/// 
+/// - ASP.NET hozzá van kötve az IIS-hez.
+/// - Az IIS hozzá van kötve a windows-hoz
+/// - Ezért, az új ASP.NET = ASP.NET Core kell, hogy többféle webkiszolgálón képes legyen futni.
+/// 
+/// - ASP.NET Core célja: multiplatformos, jól paraméterezhető webes alkalmazásokhoz fejlesztő környezet
+/// - Beérkező kéréseket feldolgozzuk, előállítsuk a választ, és kiküldjük a kérőhöz a végeredményt.
+///   (Request pipeline)
+///   
+/// - Beépített HTTP szerver: Kestrel
+/// </summary>
+
+namespace Day1.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
