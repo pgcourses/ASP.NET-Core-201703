de07c9fdf8d17c581dca0ba543d2d1ad0bae437a
gplesz <gabor.plesz@gmail.com>
2017-03-02 14:47:22 +0100

    #1: routing paraméterezés attributumokkal

--- Day1/src/Day1.API/Controllers/CourseDataController.cs
+++ Day1/src/Day1.API/Controllers/CourseDataController.cs
@@ -6,12 +6,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Day1.API.Controllers
 {
+    //[Route("api/courses")] //itt is meg tudjuk a route címet adni
+    [Route("api/[controller]")]
     public class CourseDataController : Controller
     {
         //Egy http kérés a következők egyike:
-        //GET/POST/PUT/PATCH/DELETE
+        //GET/POST/PUT/PATCH/DELETE stb.
 
-        [HttpGet("api/courses")]
+        //[HttpGet("api/courses")]
+        [HttpGet] //ha a controlleren megadtuk az elérést, akkor itt már csak a http methodot kell megadni.
         public IActionResult GetCourses()
         {
             return new JsonResult(new List<object>
