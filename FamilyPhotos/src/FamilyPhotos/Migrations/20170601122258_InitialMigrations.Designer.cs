66cf527951b09b99db0561185a5efd9c9ac88681
gplesz <gabor.plesz@gmail.com>
2017-06-01 14:28:09 +0200

    #8: létrehozzuk az első adatbázismódosító lépést (InitialMigrations), ami egy üres adatbázist csinál   + lépés: D:\Repos\ASP.NET-Core-201703\FamilyPhotos\src\FamilyPhotos>dotnet ef migrations add InitialMigrations   + lépés: D:\Repos\ASP.NET-Core-201703\FamilyPhotos\src\FamilyPhotos>dotnet ef database update

---
+++ FamilyPhotos/src/FamilyPhotos/Migrations/20170601122258_InitialMigrations.Designer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using FamilyPhotos.Data;
+
+namespace FamilyPhotos.Migrations
+{
+    [DbContext(typeof(FamilyPhotosContext))]
+    [Migration("20170601122258_InitialMigrations")]
+    partial class InitialMigrations
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ChangeDetector.SkipDetectChanges", "true")
+                .HasAnnotation("ProductVersion", "1.0.4")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
---
+++ FamilyPhotos/src/FamilyPhotos/Migrations/FamilyPhotosContextModelSnapshot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using FamilyPhotos.Data;
+
+namespace FamilyPhotos.Migrations
+{
+    [DbContext(typeof(FamilyPhotosContext))]
+    partial class FamilyPhotosContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ChangeDetector.SkipDetectChanges", "true")
+                .HasAnnotation("ProductVersion", "1.0.4")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
